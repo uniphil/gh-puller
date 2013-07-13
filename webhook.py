ef82141deb9b2c07f89f8dfb67a1393739fb74ea
Philip Schleihauf <uniphil@gmail.com>
2013-07-13 15:35:24 -0400

    lalala it owrks

--- webhook.py
+++ webhook.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python2
 
+import os, json
+
 try:
     import yaml
-    from flask import Flask
+    from sh import pwd, cd, ErrorReturnCode_128
+    from flask import Flask, request, abort
 except ImportError as e:
     e.message = "Could not import the required libraries. Have you `pip install -r requirements.txt`d them?"
     """last-ditch: is there a virtualenv to activate?"""
@@ -20,11 +23,18 @@ except ImportError as e:
     if activated:
         try:
             import yaml
-            from flask import Flask
+            from sh import pwd, cd, ErrorReturnCode_128
+            from flask import Flask, request, abort
         except ImportError:
             raise e
     else:
         raise e
+
+try:
+    from sh import git
+except ImportError as e:
+    e.message = "You need to have git installed to use this tool"
+    raise e
 
 
 def load_config(config_file='config.yaml'):
@@ -38,8 +48,8 @@ def load_config(config_file='config.yaml'):
     try:
         host, port = config['listen']['host'], config['listen']['port']
         repositories = config['repositories']
-        if not all('url' in r and 'path' in r for r in repositories):
-            raise KeyError('at least one repository is missing a url and/or path')
+        if not all('path' in r for r in repositories.values()):
+            raise KeyError('at least one repository is missing a url')
     except KeyError as e:
         e.message = 'bad config format'
         raise e
@@ -47,17 +57,61 @@ def load_config(config_file='config.yaml'):
     return (host, port), repositories
 
 
-def hookify(app, base=''):
-    @app.route(base + '/', methods=['GET', 'POST'])
+def pull(url, path):
+    here = str(pwd()).strip()
+    try:
+        cd(path)
+    except OSError:
+        print "path does not exist? {}".format(path)
+        cd(here)
+        return
+    try:
+        git.status()
+    except ErrorReturnCode_128:
+        print "{} is not a git repository!".format(path)
+        cd(here)
+        return
+    git.pull(url, 'master')
+    git.checkout('-f')
+    cd(here)
+
+
+def hookify(app, repositories):
+    @app.route('/', methods=['GET', 'POST'])
     def hook():
+        try:
+            payload = json.loads(request.form['payload'])
+            url = payload['repository']['url']
+        except KeyError:
+            print "Bad Request!"
+            abort(400)
         print 'heyheyhey'
-        return 'woo hoo hoo hoo'    
+        try:
+            repo_config = repositories[url]
+        except KeyError:
+            print "Repository not in config"
+            abort(404)
+        pull(url, **repo_config)
+        return 'cool cool'
     return app
 
 
 def gunicornify(app, host, port):
-    from gunicorn.app.base import Application
-    from gunicorn.config import Config
+    return glapp
+
+
+def run_flask(app, host, port):
+    app.run(host=host, port=port)
+
+
+def run_gunicorn(app, host, port):
+    try:
+        from gunicorn.app.base import Application
+        from gunicorn.config import Config
+    except:
+        print "warning: couldn't import gunicorn -- running flask dev server"
+        run_flask(app, host, port)
+        return
     class Glapp(Application):
         def init(self, *start):
             pass
@@ -67,17 +121,22 @@ def gunicornify(app, host, port):
     gonfig = Config()
     gonfig.set('bind', '{}:{}'.format(host, port))
     glapp.cfg = gonfig
-    return glapp
+    glapp.run()
+
+
 
 if __name__ == '__main__':
+    import sys
     (host, port), repositories = load_config()
-
     flapp = Flask(__name__)
-    hookify(flapp)
+    hookify(flapp, repositories)
 
-    try:
-        glapp = gunicornify(flapp, host, port)
-        glapp.run()
-    except ImportError:
-        print "warning: couldn't import gunicorn -- running flask dev server"
-        flapp.run(host=host, port=port)
+    server = sys.argv[1] if len(sys.argv) > 1 else 'gunicorn'
+
+    if server == 'gunicorn':
+        run_gunicorn(flapp, host, port)
+    elif server == 'debug':
+        flapp.debug = True
+        run_flask(flapp, host, port)
+    else:
+        run_flask(flapp, host, port)
